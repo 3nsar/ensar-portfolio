8bc5909195b699f5da69fc2257715fb1914481e1
3nsar <84131267+3nsar@users.noreply.github.com>
2023-06-18 23:31:53 +0200

    commit

---
+++ ensar-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div div className='App'>
+      <Navbar />
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ ensar-portfolio/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BsGithub, BsLinkedin} from "react-icons/bs"
+import {AiOutlineMail} from "react-icons/ai"
+
+const Hero = () => {
+  return (
+    <div className='hero-container'> 
+      <h1>Hi, I'm <span>Ensar</span></h1>
+      <p>Software Developer and CS Student</p>
+
+      <div className="contact-links">
+                <a target='_blank' href="https://github.com/3nsar"><BsGithub/></a>
+                <a target='_blank' href="https://www.linkedin.com/in/ensar-mehmedovic/"><BsLinkedin/></a>
+                <a target='_blank' href="mailto:ensar.m021@gmail.com"><AiOutlineMail/></a>
+            </div>
+
+    </div>
+  )
+}
+
+export default Hero
